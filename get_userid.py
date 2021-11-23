f087501935b082009bcbb4d520269b7018a19c07
Russell Pacheco <russ1487@gmail.com>
2021-11-23 12:44:33 +0900

    something else

--- get_userid.py
+++ get_userid.py
@@ -3,5 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def index():
+def index(arg1, arg2):
+    print(f"This is arg1 {arg1}")
+    print(f"This is arg2 {arg2}")
     return "<p>Hello World</p>"

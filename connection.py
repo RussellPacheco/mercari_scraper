44a63594089d8f1db2c94f7cc76ac137fb77bcdc
Russell Pacheco <russ1487@gmail.com>
2021-11-23 14:43:08 +0900

    refined check so that it lists the price along with the link. Also wrote what the message going out would look like

--- connection.py
+++ connection.py
@@ -1,13 +1,96 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+
 import os
+import re
+from time import sleep
+from typing import List, Any, Union
 
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 DRIVER_PATH = os.path.join(ROOT_PATH, "driver/chromedriver.exe")
 
-OPTIONS = Options()
-OPTIONS.headless = True
-OPTIONS.add_argument("--incognito")
 
-driver = webdriver.Chrome(options=OPTIONS, executable_path=DRIVER_PATH)
-driver.get("https://jp.mercari.com/search?keyword=nvidia%203060ti&page=1&status=on_sale")
+def _get_soup(url: str) -> BeautifulSoup:
+    OPTIONS = Options()
+    OPTIONS.headless = True
+    OPTIONS.add_argument("--incognito")
+
+    driver = webdriver.Chrome(options=OPTIONS, executable_path=DRIVER_PATH)
+    driver.get(url)
+    WebDriverWait(driver, 10).until(expected_conditions.visibility_of_any_elements_located((By.XPATH, '//*[@id="item-grid"]')))
+    soup = BeautifulSoup(driver.page_source, "lxml")
+    return soup
+
+
+class Mercari:
+
+    def fetch_all_items(
+            self,
+            keyword: str = 'clothes',
+            price_min: Union[None, int] = None,
+            price_max: Union[None, int] = None,
+            max_items_to_fetch: Union[None, int] = 100
+    ) -> List[str]:  # list of URLs.
+        items_list = []
+        for page_id in range(int(1e9)):
+            items, search_res_head_tag = self.fetch_items_pagination(keyword, page_id, price_min, price_max)
+            items_list.extend(items)
+            logger.debug(f'Found {len(items_list)} items so far.')
+
+            if max_items_to_fetch is not None and len(items_list) > max_items_to_fetch:
+                logger.debug(f'Reached the maximum items to fetch: {max_items_to_fetch}.')
+                break
+
+            if search_res_head_tag is None:
+                break
+            else:
+                search_res_head = str(search_res_head_tag.contents[0]).strip()
+                num_items = re.findall('\d+', search_res_head)
+                if len(num_items) == 1 and num_items[0] == '0':
+                    break
+            sleep(2)
+        return items_list
+
+    def fetch_items_pagination(
+            self,
+            keyword: str,
+            page_id: int = 1,
+            price_min: Union[None, int] = None,
+            price_max: Union[None, int] = None
+    ) -> Union[List[str], Any]:  # List of URLS and a HTML marker.
+        soup = _get_soup(self._fetch_url(page_id, keyword, price_min=price_min, price_max=price_max))
+        search_res_head_tag = soup.find('ul', {'id': 'item-grid'})
+        deets = [s.find("mer-item-thumbnail").attrs["price"] for s in soup.find_all('li', {"data-testid": "item-cell"})]
+        items = [s.find("a").attrs['href'] for s in soup.find_all('li', {"data-testid": "item-cell"} )]
+        items = [it if it.startswith('http') else 'https://jp.mercari.com' + it for it in items]
+
+        results = []
+
+        for i in range(len(deets)):
+            results.append([deets[i], items[i]])
+
+        return results, search_res_head_tag
+
+    def _fetch_url(
+            self,
+            page: int = 1,
+            keyword: str = 'bicycle',
+            price_min: Union[None, int] = None,
+            price_max: Union[None, int] = None
+    ):
+        url = f'https://www.mercari.com/jp/search/?keyword={keyword}'
+        url += f"&page={page}"
+        url += "&t1_category_id=7&category_id=96&t2_category_id=96&sort=created_time&order=desc&status=on_sale"
+        if price_min is not None:
+            url += f'&price_min={price_min}'
+        if price_max is not None:
+            url += f'&price_max={price_max}'
+        return url
+
+    @property
+    def name(self) -> str:
+        return 'mercari'
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
+from connection import Mercari
 import os
-from mercari import Mercari
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
 from linebot.exceptions import LineBotApiError
@@ -12,14 +12,22 @@ load_dotenv()
 
 mercari_api = Mercari()
 
-print('_' * 80)
-print(mercari_api.name)
-results = mercari_api.fetch_items_pagination(keyword="2070 -ジャンク",price_min=40000, price_max=60000)
+results = mercari_api.fetch_items_pagination(keyword="2070 -ジャンク", price_min=40000, price_max=60000)
 print(f"There are {len(results[0])} results")
 print(results[0])
 
 
-# line_bot_api = LineBotApi(os.getenv("CONNECTION_TOKEN"))
-#
-# try:
-#     line_bot_api.push_message("<USERID>", )
+line_bot_api = LineBotApi(os.getenv("CONNECTION_TOKEN"))
+
+
+# Write code for checking if found results are new against a saved json file.
+
+for item in results[0]:
+
+    message = f"Hey Russell,\n\nThere is a new item.\n\nPrice: {item[0]}円\nLink: {item[1]}"
+    print(message)
+
+    # try:
+    #     line_bot_api.push_message(os.getenv("USER_ID"), TextSendMessage(text=message))
+    # except LineBotApiError as e:
+    #     pass

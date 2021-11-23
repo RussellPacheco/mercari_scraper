79a8cb8f962092cdfebfc30ecbc921e0d506694b
Russell Pacheco <russ1487@gmail.com>
2021-11-23 13:27:34 +0900

    finished the get user_id i think

--- get_userid.py
+++ get_userid.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+        print(request.json)
+    elif request.method == "GET":
+        return render_template("index.html")
+    else:
+        return render_template("index.html")
 
 
 if __name__ == "__main__":

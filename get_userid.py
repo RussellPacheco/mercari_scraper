ba653d4f6065ee5d70c2cb899968191e098c2a81
Russell Pacheco <russ1487@gmail.com>
2021-11-23 12:47:45 +0900

    something1

--- get_userid.py
+++ get_userid.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 def index(arg1, arg2):
     print(f"This is arg1 {arg1}")
     print(f"This is arg2 {arg2}")
-    return "<p>Hello World</p>"
+    return bytes("<p>Hello, this is a test</p>", "UTF-8")
